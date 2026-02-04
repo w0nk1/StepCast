b316419b681d7db1da1feb06413b964b98462c38
w0nk1 <markus.czerwonka@gmail.com>
2026-02-04 11:23:50 +0100

    chore: restrict app command permissions

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,19 @@
 fn main() {
-    tauri_build::build()
+    let manifest = tauri_build::AppManifest::new().commands(&[
+        "greet",
+        "check_permissions",
+        "request_permissions",
+        "start_recording",
+        "pause_recording",
+        "resume_recording",
+        "stop_recording",
+        "get_steps",
+        "export_html",
+        "export_markdown",
+        "export_html_temp",
+        "discard_recording",
+    ]);
+
+    let attrs = tauri_build::Attributes::new().app_manifest(manifest);
+    tauri_build::try_build(attrs).expect("failed to run build script");
 }
